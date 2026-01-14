c035a85cc7cb315cd801eede68efdce77acef323
galdric <galdric.tingaud@gmail.com>
2026-01-14 18:48:12 +0100

    supression de contenu inutile

--- app/app/src/main/java/com/example/recipebacklog/data/recipes/FirestoreRecipeRepository.kt
+++ app/app/src/main/java/com/example/recipebacklog/data/recipes/FirestoreRecipeRepository.kt
@@ -1,19 +1,3 @@
-/*package com.example.recipebacklog.data.recipes
-
-import com.example.recipebacklog.model.Recipe
-import com.example.recipebacklog.model.RecipeStatus
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-
-
-import kotlinx.coroutines.tasks.await
-
-class FirestoreRecipeRepository {
-
-    private val db = FirebaseFirestore.getInstance()
-
-}*/
-
 package com.example.recipebacklog.data.recipes
 
 import com.example.recipebacklog.model.Recipe
